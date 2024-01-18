655d64c687c28f9c45743d99a569e118996ee1e1
Hassan Shakoor <hassanshakoor308@gmail.com>
2024-01-19 02:36:04 +0500

    Input Fields - Creating a Customizable Input Field Composable Function

--- TipApp/app/src/main/java/com/hassan/tipapp/components/Components.kt
+++ TipApp/app/src/main/java/com/hassan/tipapp/components/Components.kt
@@ -1,5 +1,6 @@
 package com.hassan.tipapp.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -16,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -36,6 +38,8 @@ fun InputField(modifier: Modifier,
         singleLine = isSingleLine,
         textStyle = TextStyle(fontSize = 18.sp,
         color = MaterialTheme.colorScheme.onBackground),
+        modifier = modifier
+            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp),
         enabled = enabled,
         keyboardOptions = KeyboardOptions(keyboardType = keyboardType,
             imeAction = imeAction),
