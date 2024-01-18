f356f090346b5b9189fe4afa657b8b97e854732b
Hassan Shakoor <hassanshakoor308@gmail.com>
2024-01-19 01:36:37 +0500

    Top Header function

--- TipApp/app/build.gradle.kts
+++ TipApp/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.hassan.tipapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.hassan.tipapp"
--- TipApp/app/src/main/java/com/hassan/tipapp/MainActivity.kt
+++ TipApp/app/src/main/java/com/hassan/tipapp/MainActivity.kt
@@ -3,44 +3,64 @@ package com.hassan.tipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hassan.tipapp.ui.theme.TipAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TipAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                Text(text ="Hello Again Sir")
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(content: @Composable () -> Unit) {
+    TipAppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background) {
+            content()
+        }
+    }
 }
 
+@Preview
+@Composable
+fun TopHeader() {
+    Surface(modifier = Modifier.fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = CircleShape.copy(all = CornerSize(12.dp)))
+        //.clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
+    ){
+        Column() {
+
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TipAppTheme {
-        Greeting("Android")
+        MyApp {
+            Text("Hello Again sir")
+        }
     }
 }
