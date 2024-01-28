3f7ec50dd452d2fb36966e2e107055c278fe93a2
Hassan Shakoor <hassanshakoor308@gmail.com>
2024-01-28 14:12:45 +0500

    Creating Round Icon Buttons

--- TipApp/app/src/main/java/com/hassan/tipapp/MainActivity.kt
+++ TipApp/app/src/main/java/com/hassan/tipapp/MainActivity.kt
@@ -6,14 +6,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -29,9 +36,11 @@ import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.hassan.tipapp.components.InputField
 import com.hassan.tipapp.ui.theme.TipAppTheme
+import com.hassan.tipapp.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -109,7 +118,10 @@ fun BillForm(modifier: Modifier,
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
         border = BorderStroke(width = 1.dp, color = Color.LightGray)
     ) {
-        Column {
+        Column(modifier = Modifier.padding(6.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start) {
+
             InputField(modifier = Modifier, valueState = totalBillState, labelId = "Enter Bill", enabled = true, isSingleLine = true,
                 onAction = KeyboardActions {
                     if(!validState) return@KeyboardActions
@@ -118,6 +130,30 @@ fun BillForm(modifier: Modifier,
                     keyboardController?.hide()
                 }
             )
+            if (validState) {
+                Row(modifier = Modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start) {
+                    Text("Split",
+                        modifier = Modifier.align(
+                            alignment = Alignment.CenterVertically
+                        ))
+                    Spacer(modifier = Modifier.width(120.dp))
+                    Row(modifier = Modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End) {
+                        RoundIconButton(
+                            imageVector = Icons.Default.Remove,
+                            onClick = { /*TODO*/ })
+
+                        RoundIconButton(
+                            imageVector = Icons.Default.Add,
+                            onClick = { /*TODO*/ })
+                    }
+                }
+            } else {
+                Box() {
+
+                }
+            }
         }
     }
 
---
+++ TipApp/app/src/main/java/com/hassan/tipapp/widgets/RoundIconButton.kt
@@ -0,0 +1,45 @@
+package com.hassan.tipapp.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButtonDefaults.elevation
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+val IconButtonSizeModifier = Modifier.size(40.dp)
+@Composable
+fun RoundIconButton(
+    modifier: Modifier = Modifier,
+    imageVector: ImageVector,
+    onClick: () -> Unit,
+    tint: Color = Color.Black.copy(alpha =0.8f),
+    backgroundColor: Color = MaterialTheme.colorScheme.background,
+    elevation: Dp = 4.dp
+) {
+    Card(modifier = modifier
+        .padding(all = 4.dp)
+        .clickable { onClick.invoke() }
+        .then(IconButtonSizeModifier),
+        shape = CircleShape,
+        colors = CardDefaults.cardColors(
+            contentColor = backgroundColor
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = elevation
+        )){
+        Icon(imageVector = imageVector, contentDescription = "Plus or minus icon",
+            tint = tint)
+    }
+}
