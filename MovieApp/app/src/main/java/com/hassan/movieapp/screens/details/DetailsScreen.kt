3cd13ca4af2afc9891d78f529915be64143fc621
Hassan Shakoor <hassanshakoor308@gmail.com>
2024-02-06 12:40:38 +0500

    Navigating back on back button

--- MovieApp/app/src/main/java/com/hassan/movieapp/screens/details/DetailsScreen.kt
+++ MovieApp/app/src/main/java/com/hassan/movieapp/screens/details/DetailsScreen.kt
@@ -2,14 +2,18 @@ package com.hassan.movieapp.screens.details
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -22,7 +26,12 @@ fun DetailsScreen(navController: NavController,
         Column(horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center) {
             Text(text = movieData.toString(), style = MaterialTheme.typography.headlineSmall)
-
+            Spacer(modifier = Modifier.height(23.dp))
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text(text = "Go Back")
+            }
         }
     }
 }
