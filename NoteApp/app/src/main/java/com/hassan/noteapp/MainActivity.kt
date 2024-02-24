25f2c9232d4d376bfe8156d3d4f549664a241ff0
Hassan Shakoor <hassanshakoor308@gmail.com>
2024-02-24 15:08:20 +0500

    Creating a Note Row and Showing Dummy Data

--- NoteApp/app/src/main/java/com/hassan/noteapp/MainActivity.kt
+++ NoteApp/app/src/main/java/com/hassan/noteapp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.hassan.noteapp.data.NoteDataSource
 import com.hassan.noteapp.screen.NoteScreen
 import com.hassan.noteapp.ui.theme.NoteAppTheme
 
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NoteScreen(notes = emptyList(), onAddNote = {},
+                    NoteScreen(notes = NoteDataSource().loadNotes(), onAddNote = {},
                         onRemove = {})
                 }
             }
--- NoteApp/app/src/main/java/com/hassan/noteapp/screen/NoteScreen.kt
+++ NoteApp/app/src/main/java/com/hassan/noteapp/screen/NoteScreen.kt
@@ -1,12 +1,20 @@
 package com.hassan.noteapp.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
@@ -18,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,7 +34,9 @@ import androidx.compose.ui.unit.dp
 import com.hassan.noteapp.R
 import com.hassan.noteapp.components.NoteButton
 import com.hassan.noteapp.components.NoteInputText
+import com.hassan.noteapp.data.NoteDataSource
 import com.hassan.noteapp.model.Note
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -84,11 +95,44 @@ fun NoteScreen(notes: List<Note>, onAddNote: (Note) -> Unit, onRemove: (Note) ->
                     }
                 })
         }
+        Divider(modifier = Modifier.padding(10.dp))
+        LazyColumn {
+            items(notes) { note ->
+                NoteRow(note = note, onNoteClicked = {})
+            }
+        }
     }
+}
+
+@Composable
+fun NoteRow(modifier: Modifier = Modifier,
+            note: Note,
+            onNoteClicked: (Note) -> Unit){
+    Surface(
+        modifier
+            .padding(4.dp)
+            .clip(RoundedCornerShape(topEnd = 33.dp, bottomStart = 33.dp))
+            .fillMaxWidth(),
+        color = Color(0xFFDFE6EB),
+        shadowElevation = 6.dp) {
+        Column(
+            modifier
+                .clickable { }
+                .padding(horizontal = 14.dp, vertical = 6.dp),
+            horizontalAlignment = Alignment.Start) {
+            Text(text = note.title, style = MaterialTheme.typography.titleLarge)
+            Text(text = note.description, style = MaterialTheme.typography.titleMedium)
+            Text(text = note.entryDate.format(DateTimeFormatter.ofPattern("EEE, d MMM")),
+                style = MaterialTheme.typography.titleSmall)
+                        
+
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun NoteScreenPreview() {
-    NoteScreen(notes = emptyList(), onAddNote = {}, onRemove = {})
+    NoteScreen(notes = NoteDataSource().loadNotes(), onAddNote = {}, onRemove = {})
 }
